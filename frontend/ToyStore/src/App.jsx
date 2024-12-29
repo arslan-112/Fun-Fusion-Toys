7a341df4151dcb830a01ab3ee1fbf647bd44e098
Arslan <ijazarslan372@gmail.com>
2024-12-29 17:36:57 +0500

    Made some changes in packages

--- frontend/ToyStore/src/App.jsx
+++ frontend/ToyStore/src/App.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AboutUs } from "./Pages/AboutUs";
 import { ContactUs } from "./Pages/ContactUs";
 import { Products } from "./Pages/Products";
-import { Shop } from "./Pages/shop";
-import { Cart } from "./Pages/cart";
+import { Shop } from "./Pages/Shop";
+import { Cart } from "./Pages/Cart";
 import { LoginPage } from "./Pages/LoginPage";
 import { Footer } from "./Components/Footer/Footer";
 import { ProductDescription } from "./Pages/ProductDescription";
 import { CheckoutPage } from "./Pages/Checkout";
-import  Myorders  from "./Pages/Myorders";
+import Myorders from "./Pages/Myorders";
 import { TopBar } from "./Components/TopBar/TopBar";
 
 const App = () => {
@@ -31,17 +31,20 @@ const App = () => {
             path="/*"
             element={
               <div>
-                <TopBar/>
+                <TopBar />
                 <Navbar />
                 <Routes>
                   <Route path="/home" element={<Shop />} />
                   <Route path="/AboutUs" element={<AboutUs />} />
                   <Route path="/ContactUs" element={<ContactUs />} />
-                  <Route path="/Shop" element={<Products />}/>
-                  <Route path="/Shop/:productId" element={<ProductDescription/>} />
+                  <Route path="/Shop" element={<Products />} />
+                  <Route
+                    path="/Shop/:productId"
+                    element={<ProductDescription />}
+                  />
                   <Route path="/Cart" element={<Cart />} />
-                  <Route path="/checkout" element={<CheckoutPage/>} />
-                  <Route path="/MyOrders" element={<Myorders/>} />
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/MyOrders" element={<Myorders />} />
                 </Routes>
                 <Footer />
               </div>
--- frontend/ToyStore/src/Pages/Shop.jsx
+++ frontend/ToyStore/src/Pages/Shop.jsx
@@ -1,13 +1,13 @@
-import { Exclusive } from "../Components/Exclusive/Exclusive"
-import { Middle } from "../Components/Middle/middle"
-import { Newarrivals } from "../Components/NewArrivals/Newarrivals"
+import { Exclusive } from "../Components/Exclusive/Exclusive";
+import { Middle } from "../Components/Middle/Middle";
+import { Newarrivals } from "../Components/NewArrivals/Newarrivals";
 
 export const Shop = () => {
   return (
     <div>
-        <Middle/>
-        <Newarrivals/>
-        <Exclusive/>
+      <Middle />
+      <Newarrivals />
+      <Exclusive />
     </div>
-  )
-}
+  );
+};
