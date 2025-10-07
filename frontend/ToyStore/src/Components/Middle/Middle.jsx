ab4ade55144b68bd8a1d881a3784e31126290650
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2025-10-07 19:54:08 +0500

    Update Middle.jsx

--- frontend/ToyStore/src/Components/Middle/Middle.jsx
+++ frontend/ToyStore/src/Components/Middle/Middle.jsx
@@ -8,7 +8,7 @@ export const Middle = () => {
       <div className="overlay">
         <img className="imga" src={slider} alt="Slider Image" />
         <div className="text-overlay">
-          <h1>Welcome to Fun Fusion Toys</h1>
+          <h1>Welcome to EliteToys by Arslan</h1>
           <p>Discover the joy of play with our amazing collection of toys!</p>
           <Link to = "/Shop"><button>Shop Now</button></Link>
         </div>
